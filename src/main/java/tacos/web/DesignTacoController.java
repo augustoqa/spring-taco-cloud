670e12f8b06b17113789d24904a6de4c2bead600
Augusto Acual <augustoacual@gmail.com>
2023-03-21 10:45:31 -0600

    Validating form input

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,17 +1,25 @@
 package tacos.web;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.TacoOrder;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -59,7 +67,14 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(
+    		@Valid Taco taco, Errors errors, 
+    		@ModelAttribute TacoOrder tacoOrder) {
+    	
+    	if (errors.hasErrors()) {
+    		return "design";
+    	}
+    	
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,13 +1,16 @@
 package tacos.web;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
 
 @Slf4j
@@ -22,7 +25,14 @@ public class OrderController {
 	}
 
 	@PostMapping
-	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+	public String processOrder(
+			@Valid TacoOrder order, Errors errors, 
+			SessionStatus sessionStatus) {
+		
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
 		log.info("Order submitted: {}", order);
 		sessionStatus.setComplete();
 
